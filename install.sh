56d322e9d6eab09f49b1d96d7c6164af03d84173
Heath Dutton ☕ <heathdutton@gmail.com>
2015-11-13 14:40:19 -0500

    Updated installer with chmod commands.

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@
 echo "Installing/updating ac-tools"
 cd /usr/local/bin
 
-curl -o ac-logs https://raw.githubusercontent.com/clwdev/ac-tools/master/ac-logs/ac-logs.sh
-curl -o ac-quiet https://raw.githubusercontent.com/clwdev/ac-tools/master/ac-quiet/ac-quiet.sh
-curl -o ac-update https://raw.githubusercontent.com/clwdev/ac-tools/master/ac-update/ac-update.sh
+curl -sso ac-logs https://raw.githubusercontent.com/clwdev/ac-tools/master/ac-logs/ac-logs.sh
+chmod 0755 ac-logs
+
+curl -sso ac-quiet https://raw.githubusercontent.com/clwdev/ac-tools/master/ac-quiet/ac-quiet.sh
+chmod 0755 ac-quiet
+
+curl -sso ac-update https://raw.githubusercontent.com/clwdev/ac-tools/master/ac-update/ac-update.sh
+chmod 0755 ac-update
 
 echo "Installation complete"
 cd -
